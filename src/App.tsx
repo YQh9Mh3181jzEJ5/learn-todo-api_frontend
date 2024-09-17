d49e32cc406d4c9c9fd75e1f66f0c9697d8e24c6
X58okYW8B54e <113420721+YQh9Mh3181jzEJ5@users.noreply.github.com>
2024-09-17 22:14:49 +0900

    create: base ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
+import { Box, Center, CheckboxGroup, Text } from "@chakra-ui/react";
 import Task from "./components/Task";
 
 const App = () => {
   return (
-    <>
-      <h1>タスク一覧</h1>
-      <Task name="買い物" />
-      <Task name="ランニング" />
-      <Task name="プログラミングの勉強" />
-    </>
+    <Box>
+      <Center>
+        <Box>
+          <Box mb="24px">
+            <Text fontSize="24px" fontWeight="bold">
+              タスク一覧
+            </Text>
+          </Box>
+          <CheckboxGroup>
+            <Task name="買い物" />
+            <Task name="ランニング" />
+            <Task name="プログラミングの勉強" />
+          </CheckboxGroup>
+        </Box>
+      </Center>
+    </Box>
   );
 };
 
